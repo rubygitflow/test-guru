2bc326b02c6938c5cb81eaebcc26d362d5d51826
rubygitflow <nrubygitflow@gmail.com>
2020-05-25 00:51:23 +0300

    change module User::Auth on has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,10 @@
 class User < ApplicationRecord
 
-  include Auth
-
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: "Test", foreign_key: "author_id"
 
-  # has_secure_password # replaced include Auth
+  has_secure_password # replaced include Auth
 
   def test_by_level(level)
     tests.where(level: level)
