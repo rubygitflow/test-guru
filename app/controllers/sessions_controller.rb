383d5d30e0a429f72ea7ab7e613a30be729c7d21
rubygitflow <nrubygitflow@gmail.com>
2020-05-28 20:09:23 +0300

    fix target_url

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:notice] = "Wellcome, #{user.name}!"
-      redirect_to cookies[:target_url]
+      redirect_to cookies[:target_url] || root_path
     else
       flash.now[:alert] = 'Verify your Email or Password'
       render :new
