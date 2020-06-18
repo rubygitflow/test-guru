3786c3d900e6570a60bcd4dce319b90af6b3dff9
rubygitflow <nrubygitflow@gmail.com>
2020-06-18 05:27:56 +0300

    pure JS progress bar

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -2,8 +2,4 @@ module TestPassagesHelper
   def success?(completed_percent)
     completed_percent >= TestPassage::GOOD_PROGRESS
   end
-  
-  def progress_bar_width(test_passage)
-    test_passage.current_question_number.to_f / test_passage.test.questions.size * 100
-  end
 end
