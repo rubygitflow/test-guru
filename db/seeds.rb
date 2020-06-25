193fe75da9c22c5260f1a61379cf1b2aeb4f6041
rubygitflow <nrubygitflow@gmail.com>
2020-06-25 20:50:19 +0300

    refresh seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,4 @@
-users = User.create!([{name: "Admin", email: "it.architect@yahoo.com", 
-                       password: '123456', type: "Admin",
-                       last_name: 'Lastmin'}])
+user = User.where(name: 'architect').take
 
 category_sections = %w[Ruby RoR JavaScript React HTML CSS]
 categories = []
@@ -8,43 +6,44 @@ category_sections.each do |section|
   categories << Category.create!(title: section)
 end
 
+if user 
+  tests_data = [
+    {title: 'Ruby beginner', level: 1, category_id: categories[0].id, author_id: user.id},
+    {title: 'Ruby master', level: 3, category_id:  categories[0].id, author_id: user.id},
+    {title: 'RoR beginner', level: 1, category_id:  categories[1].id, author_id: user.id},
+    {title: 'RoR master', level: 3, category_id:  categories[1].id, author_id: user.id},
+    {title: 'JavaScript programmer', level: 2, category_id:  categories[2].id, author_id: user.id},
+    {title: 'React programmer', level: 3, category_id:  categories[2].id, author_id: user.id},
+    {title: 'HTML coder', level: 1, category_id:  categories[3].id, author_id: user.id},
+    {title: 'CSS designer', level: 2, category_id:  categories[4].id, author_id: user.id}
+  ]
 
-tests_data = [
-  {title: 'Ruby beginner', level: 1, category_id: categories[0].id, author_id: users[0].id},
-  {title: 'Ruby master', level: 3, category_id:  categories[0].id, author_id: users[0].id},
-  {title: 'RoR beginner', level: 1, category_id:  categories[1].id, author_id: users[0].id},
-  {title: 'RoR master', level: 3, category_id:  categories[1].id, author_id: users[0].id},
-  {title: 'JavaScript programmer', level: 2, category_id:  categories[2].id, author_id: users[0].id},
-  {title: 'React programmer', level: 3, category_id:  categories[2].id, author_id: users[0].id},
-  {title: 'HTML coder', level: 1, category_id:  categories[3].id, author_id: users[0].id},
-  {title: 'CSS designer', level: 2, category_id:  categories[4].id, author_id: users[0].id}
-]
-
-tests = []
-tests_data.each do |test_data|
-  tests << Test.create!(test_data)
-end
-
-
-length = 50
-questions = []
-tests.each do |test|
-  question_number = 1
-  while question_number < 14 do
-    questions << Question.create!(body: "#{rand(36**length).to_s(36)} ?", 
-                                  test: test)
-    question_number += 1
+  tests = []
+  tests_data.each do |test_data|
+    tests << Test.create!(test_data)
   end
-end
 
-length = 10
-questions.each do |question|
-  answer_count = 1
-  while answer_count < 4
-    random_boolean = 
-    Answer.create!(body: "#{rand(16**length).to_s(16)}",
-                   correct: [true, false].sample, 
-                   question: question)
-    answer_count += 1
+
+  length = 50
+  questions = []
+  tests.each do |test|
+    question_number = 1
+    while question_number < 14 do
+      questions << Question.create!(body: "#{rand(36**length).to_s(36)} ?", 
+                                    test: test)
+      question_number += 1
+    end
+  end
+
+  length = 10
+  questions.each do |question|
+    answer_count = 1
+    while answer_count < 4
+      random_boolean = 
+      Answer.create!(body: "#{rand(16**length).to_s(16)}",
+                     correct: [true, false].sample, 
+                     question: question)
+      answer_count += 1
+    end
   end
 end
