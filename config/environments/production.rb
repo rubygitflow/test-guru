b064b6a01193f22b15853d4253caa7d6f5a1c747
rubygitflow <nrubygitflow@gmail.com>
2020-06-25 14:43:37 +0300

    check yahoo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,10 +66,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.mail.yahoo.com',
-    port: 465,
+    port: 587,
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
-    authentication: :login,
+    authentication: :plain,
+    ssl: true,
     enable_starttls_auto: true
   }
 
