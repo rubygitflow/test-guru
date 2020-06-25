7b946c5e631f0c3d8261c80684d50729fc88d3e6
rubygitflow <nrubygitflow@gmail.com>
2020-06-25 14:50:03 +0300

    fix yahoo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,6 @@ Rails.application.configure do
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
     authentication: :plain,
-    ssl: true,
     enable_starttls_auto: true
   }
 
