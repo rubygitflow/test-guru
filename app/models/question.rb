2f3bd853ea9d5e6ecc1a244a8a4025a345a6ba62
rubygitflow <nrubygitflow@gmail.com>
2020-06-26 18:14:28 +0300

    fix delete-functions

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,9 @@
 class Question < ApplicationRecord
   belongs_to :test
-  has_many :answers
-  has_many :gists
+  # http://rusrails.ru/active-record-associations#dependent
+  # https://guides.rubyonrails.org/association_basics.html
+  has_many :answers, dependent: :destroy
+  has_many :gists, dependent: :destroy
   
   validates :body, presence: true
 end
