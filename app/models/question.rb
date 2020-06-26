401aae0bbb3d5674f4cb89aa345b367607879615
rubygitflow <nrubygitflow@gmail.com>
2020-06-26 22:15:29 +0300

    add passed_tests dependent: :nullify

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,6 +4,9 @@ class Question < ApplicationRecord
   # https://guides.rubyonrails.org/association_basics.html
   has_many :answers, dependent: :destroy
   has_many :gists, dependent: :destroy
+  has_many :passed_tests, class_name: "TestPassage",
+                          foreign_key: 'current_question_id', 
+                          dependent: :nullify
   
   validates :body, presence: true
 end
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -1,7 +1,9 @@
 class TestPassage < ApplicationRecord
   belongs_to :user
   belongs_to :test
-  belongs_to :current_question, class_name: 'Question', optional: true, dependent: :destroy
+  belongs_to :current_question, class_name: 'Question',
+                                foreign_key: 'current_question_id', 
+                                optional: true
 
   before_validation :before_validation_set_first_question, on: :create
   before_update :before_update_set_next_question
@@ -18,6 +20,11 @@ class TestPassage < ApplicationRecord
 
   def completed?
     current_question.nil?
+  end
+
+  def accept!(answer_ids)
+    self.correct_question += 1 if correct_answer?(answer_ids)
+    self.save!
   end
 
   def accept!(answer_ids)
