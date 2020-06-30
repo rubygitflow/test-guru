a919e505c5f3248e578b66c1b8fc2672707e88b1
rubygitflow <nrubygitflow@gmail.com>
2020-06-30 15:40:07 +0300

    fix Migration

--- db/migrate/20200630080311_change_data_type_for_value_in_badges.rb
+++ db/migrate/20200630080311_change_data_type_for_value_in_badges.rb
@@ -3,4 +3,8 @@ class ChangeDataTypeForValueInBadges < ActiveRecord::Migration[6.0]
     #change column type form int to  string
     change_column :badges, :value, :string
   end
+  def down
+    #change column type form int back to  integer
+    change_column :badges, :value, :integer
+  end
 end
